4f6f9a3014cbb7f8ce658ba6896158b23ab81ea0
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2017-04-25 23:17:05 +0300

    set proxy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -124,7 +124,8 @@ function images() {
 // Start a server with BrowserSync to preview the site in
 function server(done) {
   browser.init({
-    server: PATHS.dist, port: PORT
+    //server: PATHS.dist, port: PORT
+    proxy: 'http://europraxis.dev'
   });
   done();
 }
